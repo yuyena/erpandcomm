e346398d536883ed372edc5014343d7b2c27bc37
user <user@DESKTOP-9RMOC1A>
2025-07-14 17:26:05 +0900

    product_yn_commit

--- erpandcomm/src/main/java/kr/spring/product/controller/ProductController.java
+++ erpandcomm/src/main/java/kr/spring/product/controller/ProductController.java
@@ -98,40 +98,43 @@ public class ProductController {
 	    log.debug("=== 제품 목록 조회 완료 ===");
 	    return "views/product/productView";  
 	}
-	//게시판 글등록 폼
+	//상품 등록 폼
 	@PreAuthorize("isAuthenticated()")
 	@GetMapping("/register")
-	public String form() {
+	public String form(Model model) {
+		List<Map<String, Object>> categoryList = productService.selectCategoryList();
+		model.addAttribute("categoryList", categoryList);
+		
 		return "views/product/productRegister";
 	}
 	
 	//글등록 처리
-	@PreAuthorize("isAuthenticated()")
-	@PostMapping("/register")
-	public String submit(@Valid ProductVO productVO,
-			             BindingResult result,
-			             HttpServletRequest request,
-			             @AuthenticationPrincipal
-			             PrincipalDetails principal,
-			             Model model) 
-			            		 throws IllegalStateException, 
-			            		               IOException {
-		
-		log.debug("<<게시판 글등록>> : {}",productVO);
-		
-		//유효성 체크 결과 오류가 있으면 폼 호출
-		if(result.hasErrors()) {
-			//유효성 체크 결과 오류 필드 출력
-			ValidationUtil.printErrorFields(result);
-			return form();
-		}		
-		
-		//상품등록
-		productService.insertProduct(productVO);
-		
-		model.addAttribute("message", "상품을 정상적으로 등록했습니다.");
-		model.addAttribute("url", request.getContextPath()+"/product/productView");
-		
-		return "views/common/resultAlert";
-	}
+//	@PreAuthorize("isAuthenticated()")
+//	@PostMapping("/register")
+//	public String submit(@Valid ProductVO productVO,
+//			             BindingResult result,
+//			             HttpServletRequest request,
+//			             @AuthenticationPrincipal
+//			             PrincipalDetails principal,
+//			             Model model) 
+//			            		 throws IllegalStateException, 
+//			            		               IOException {
+//		
+//		log.debug("<<게시판 글등록>> : {}",productVO);
+//		
+//		//유효성 체크 결과 오류가 있으면 폼 호출
+//		if(result.hasErrors()) {
+//			//유효성 체크 결과 오류 필드 출력
+//			ValidationUtil.printErrorFields(result);
+//			return "views/product/productRegister";
+//		}		
+//		
+//		//상품등록
+//		productService.insertProduct(productVO);
+//		
+//		model.addAttribute("message", "상품을 정상적으로 등록했습니다.");
+//		model.addAttribute("url", request.getContextPath()+"/product/productView");
+//		
+//		return "views/common/resultAlert";
+//	}
 }
