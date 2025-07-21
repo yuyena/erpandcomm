ec76b54e997983a78716f1e5b14e438af148d040
SIST <SIST@DESKTOP-AFAUJVA>
2025-07-21 16:25:52 +0900

    호랑나비

--- erpandcomm/src/main/java/kr/spring/purchase/vo/PurchaseOrderVO.java
+++ erpandcomm/src/main/java/kr/spring/purchase/vo/PurchaseOrderVO.java
@@ -13,10 +13,12 @@ import java.util.List;
 public class PurchaseOrderVO {
     private long purchase_order_num;   // 구매 식별 번호
     private long supplier_num;         // 공급 업체 식별 번호
+    private String client_name;        // 공급처명(조인)
     private String supplier_name;      // 공급업체명(조인)
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     private Date order_date;           // 주문일
     private double total_price;        // 총 구매금액
     private long emp_num;              // 구매 담당 직원
+    private String emp_name;            // 담당자명(조인)
     private List<PurchaseOrderDetailVO> details; // 주문 상세 리스트
 } 
